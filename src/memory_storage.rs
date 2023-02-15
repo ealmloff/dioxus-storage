d6494f9b1e758dd1f7db1f0ac9294b2cfafb6200
= <evanalmloff@gmail.com>
2023-02-14 18:55:46 -0600

    create storage

--- src/memory_storage.rs
+++ src/memory_storage.rs
@@ -12,39 +12,28 @@ use std::{
 };
 use web_sys::window;
 
-use crate::storage::{serde_to_string, try_serde_from_string};
+use crate::storage::{serde_to_string, try_serde_from_string, StorageBacking};
 
 static mut CACHED: Lazy<Storage> = Lazy::new(|| {
     // try read from temp storage
     Storage::default()
 });
 
-#[derive(Default)]
-struct Storage {
-    data: RwLock<HashMap<String, Vec<u8>>>,
-}
+pub struct MemoryStorage;
 
-impl Drop for Storage {
-    fn drop(&mut self) {
-        let serialized = serde_to_string(&self.data);
+impl<K: Serialize + for<'a> Deserialize<'a>> StorageBacking for MemoryStorage {
+    type Key = K;
+
+    fn get<T: for<'a> Deserialize<'a>>(key: &Self::Key) -> Option<T> {}
+
+    fn set<T: Serialize>(key: Self::Key, value: &T) {
+        todo!()
     }
 }
 
-#[derive(Clone)]
-pub struct StorageEntry<T: Serialize + for<'a> Deserialize<'a>> {
-    data: T,
-    created: std::time::Instant,
-}
-
-impl<T: Display + Serialize + for<'a> Deserialize<'a>> StorageEntry<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.data.fmt(f)
-    }
-}
-
-pub fn temp<T: Serialize + for<'a> Deserialize<'a>>(
+pub fn use_temp<T: Serialize + for<'a> Deserialize<'a>>(
+    cx: &ScopeState,
     key: &'static str,
-    timeout: Duration,
     init: impl FnOnce() -> T,
 ) -> T {
     #[cfg(target_arch = "wasm32")]
